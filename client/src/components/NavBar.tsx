6e91aebecdc3331e27b613154f38328d965f7bc1
ChanMinKhant <jiyonfire@gmail.com>
2025-02-07 11:06:56 +0630

    asdf

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+// import './App.css'
+
+export default function NavBar() {
+  return (
+    <nav>
+      <h2>My YouTube Clone</h2>
+    </nav>
+  );
+}
